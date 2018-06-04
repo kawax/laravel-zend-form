054dc63f2993742c80a8cb88c6b08164c2b60249
kawax <kawaxbiz@gmail.com>
2018-06-04 11:11:29 +0900

    stub

--- src/Commands/FormMakeCommand.php
+++ src/Commands/FormMakeCommand.php
@@ -3,7 +3,6 @@
 namespace Revolution\ZendForm\Commands;
 
 use Illuminate\Console\GeneratorCommand;
-use Symfony\Component\Console\Input\InputOption;
 
 class FormMakeCommand extends GeneratorCommand
 {
